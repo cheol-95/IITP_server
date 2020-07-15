9fdb2fa99c90b1665fd8fe30361acc9e7f8de16c
cheol-95 <rkdcjf0122@gmail.com>
2020-07-16 05:32:10 +0900

    update server

--- regist_server/tasks.py
+++ regist_server/tasks.py
@@ -4,7 +4,7 @@ import pymongo
 from pymongo import MongoClient
 from bson.objectid import ObjectId
 from bson import json_util
-
+import time
 from python_json_config import ConfigBuilder
 config = ConfigBuilder().parse_config('../config.json')
 
@@ -12,18 +12,17 @@ HOST = config.server.host
 PORT = config.server.port
 DB=0
 
+
 app = Celery('tasks', backend='redis://' + HOST+ ':' + PORT.celery +'/' + str(DB), broker='redis://' + HOST +':' + PORT.celery +'/' + str(DB))
 conn = MongoClient(HOST+':'+PORT.mongo)
 db = conn.sensor
 
-# 센서 정보 등록
 @app.task
 def regist(data):
     app.send_task('tasks.insert_key', args=[json.dumps(data)])
     _id  = db.device.insert(data)
     return str(_id)
 
-# 센서 키값 등록
 @app.task
 def insert_key(data):
     data = json.loads(data)
@@ -32,7 +31,6 @@ def insert_key(data):
         doc = { 'key':i , 'sensor':data }
         db.keys.insert(doc)
 
-# 등록되어있는 키값 출력
 @app.task
 def kfind(value):
     kList = []
@@ -48,40 +46,34 @@ def kfind(value):
             kList.append(tmp)
     return str(json.dumps(kList, default=json_util.default))
 
-# 센서 정보 검색
 @app.task
 def find(key, value):
-    if(key == None): # 복수
+    if(key == None):
         li = list(db.device.find({}))
     else:
-        if(key == '_id'): # 단수
+        if(key == '_id'):
             value = ObjectId(value)
         li = list(db.device.find({ key:value }))
     return str(json.dumps(li, default=json_util.default))
 
-# 센서 정보 수정
 @app.task
 def update(key, value, skey, svalue):
     db.device.update_one({ key:value },{'$set':{ skey: svalue }})
     result = db.device.find_one({ key:value })
     return json.dumps(result,default=json_util.default)
 
-# 센서 정보 삭제
 @app.task
 def delete(key, value):
     if(key == None):
         db.device.drop()
         db.keys.drop()
     else:
-        tmp = list(db.device.find({ key:value }))
-        print(tmp)
+        tmp = list(db.device.find({ key:value }, {'_id':0 }))
         for i in tmp:
             app.send_task('tasks.delete_key', args=[json.dumps(i)])
-        #result = db.device.remove({ key:value })
+        result = db.device.remove({ key:value })
 
-# 센서 키값 삭제
 @app.task
 def delete_key(data):
     data = json.loads(data)
-    del data['_id']
     db.keys.remove({ 'sensor':data })
--- tasks.py
+++ tasks.py
@@ -9,47 +9,58 @@ from bson.objectid import ObjectId
 from bson import json_util
 import heapq
 from python_json_config import ConfigBuilder
-
 config = ConfigBuilder().parse_config('../../config.json')
 
 HOST = config.server.host
 PORT = config.server.port
-DB = 0
-
+DB=0
 app = Celery(
-    'tasks', backend='redis://' + HOST + ':' + PORT.celery + '/' + str(DB),
-    broker='redis://' + HOST + ':' + PORT.redis + '/' + str(DB)
-)
-conn = MongoClient(HOST + ':' + PORT.mongo)
+        'tasks', backend='redis://' + HOST+ ':' + PORT.celery +'/' + str(DB),
+        broker='redis://' + HOST +':' + PORT.redis +'/' + str(DB)
+        )
+conn = MongoClient(HOST+':'+PORT.mongo)
 db = conn.sensor
-init = {"cnt": 0, "heap": [], "lookup": [], "max_length": config.fileset.summary_count, "make_heap": -1, "insert": -1}
 
+init = {"cnt":0, "heap":[], "lookup":[], "max_length":config.fileset.summary_count, "make_heap": -1, "insert": -1}
+
+'''
+def _main():
+    POOL = redis.ConnectionPool(host=HOST, port=PORT.redis, db=DB)
+    try:
+        rconn = redis.Redis(connection_pool=POOL)
+        rconn.ping()
+        print ("Redis is connected!")
+    except redis.ConnectionError:
+        print ("Redis connection error!")
+'''
 POOL = redis.ConnectionPool(host=HOST, port=PORT.redis, db=DB)
 try:
     rconn = redis.Redis(connection_pool=POOL)
     rconn.ping()
-    print("Redis is connected!")
+    print ("Redis is connected!")
 except redis.ConnectionError:
-    print("Redis connection error!")
+   print ("Redis connection error!")
 
-# celery 연결 테스트
 @app.task
 def ping_celery():
     result = 'connected'
     return str(json.dumps(result, default=json_util.default))
 
-# 데이터 삽입 / mongodb-sensor-data
 @app.task
 def insert(dname, value):
-    doc = {"date": int(datetime.now().strftime('%Y%m%d%H%M%S%f')[:19]), "dev_name": dname, "data": value}
+    doc = {"date": int(datetime.now().strftime('%Y%m%d%H%M%S%f')[:19]), "dev_name": dname, "data":value}
     app.send_task('tasks.make_heap', args=[json.dumps(doc)])
     db.data.insert(doc)
 
-# 우선순위 큐를 사용해 heap구조로 저장 / redis['센서 명']
 @app.task
 def make_heap(doc):
     doc = json.loads(doc)
+    #rconn = redis.Redis(connection_pool=POOL)
     data = json.loads(rconn.get(doc["dev_name"]))
+    '''
+    if data['cnt']+1 > data['max_length']:
+        del data['heap'][data['heap'].index([data['lookup'].pop(0), data['cnt']-data['max_length']])]
+    '''
     data['lookup'].append(doc['data'])
     heapq.heappush(data['heap'], [doc['data'], data['cnt']])
     data['cnt'] += 1
@@ -57,8 +68,8 @@ def make_heap(doc):
     data['insert'] = data['cnt']
     rconn.set(doc["dev_name"], json.dumps(data))
 
-    if data['cnt'] % data['max_length'] == 0:
-        rconn.set(doc["dev_name"] + 'summary', json.dumps(data))
+    if data['cnt']%data['max_length'] == 0:
+        rconn.set(doc["dev_name"]+'summary', json.dumps(data))
 
         summary_data = [doc, data['heap'], data['lookup']]
         app.send_task('tasks.insert_summary', args=[json.dumps(summary_data)])
@@ -66,81 +77,74 @@ def make_heap(doc):
         init["cnt"] = data['cnt']
         init["make_heap"] = data['cnt']
         rconn.set(doc["dev_name"], json.dumps(init))
+    print(json.loads(rconn.get(doc["dev_name"])))
 
-# 누적 데이터 요약정보 삽입 / mongodb-sensor-summary
 @app.task
 def insert_summary(summary_data):
     doc, heap, lookup = json.loads(summary_data)
     del doc['data']
-    doc['max'] = heapq.nlargest(1, heap)[0][0]
-    doc['min'] = heapq.nsmallest(1, heap)[0][0]
-    doc['avg'] = sum(lookup) / len(lookup)
+    doc['max'] = heapq.nlargest(1,heap)[0][0]
+    doc['min'] = heapq.nsmallest(1,heap)[0][0]
+    doc['avg'] = sum(lookup)/len(lookup)
     db.summary.insert(doc)
     print('insert_summary')
 
-# 센서 데이터 출력 / mongodb-sensor-data
+
 @app.task
 def maxmin(dname, flag):
+    #rconn = redis.Redis(connection_pool=POOL)
     if flag == True:
-        rname = dname + 'summary'
+        rname = dname+'summary'
         tmp = json.loads(rconn.get(rname))
     else:
         tmp = json.loads(rconn.get(dname))
     heap = tmp["heap"]
     length = len(tmp["lookup"])
+    print(sum(tmp["lookup"]), length)
 
-    avg = sum(tmp["lookup"]) / length
-    res = [heap, heapq.nlargest(1, heap)[0][0], heapq.nsmallest(1, heap)[0][0], avg]
+    avg = sum(tmp["lookup"])/length
+    res = [heap, heapq.nlargest(1,heap)[0][0], heapq.nsmallest(1,heap)[0][0], avg]
     return json.dumps(res)
 
-# summary 출력 / mongodb-sensor-summary
 @app.task
 def summary():
-    doc_list = list(db.summary.find({}, {'_id': 0}))
+    doc_list = list(db.summary.find( {}, {'_id':0}))
     return str(json.dumps(doc_list, default=json_util.default))
 
-# DB에 등록되어있는 센서 목록 출력
 @app.task
 def get_sname():
-    li = list(db.device.find({}, {'dev_name': 1}))
+    li = list(db.device.find( {}, {'dev_name':1} ))
     names = []
     for sensor in li:
         names.append(sensor['dev_name'])
     return str(json.dumps(names, default=json_util.default))
 
-# data 검색 / mongodb-sensor-data
 @app.task
 def find(tf):
     if tf == None:
-        doc_list = list(db.data.find({}, {'_id': 0}))
+        doc_list = list(db.data.find( {},{'_id':0}))
     else:
-        doc_list = list(db.data.find({'error': tf}, {'_id': 0}))
+        doc_list = list(db.data.find({ 'error':tf },{ '_id':0 }))
     return str(json.dumps(doc_list, default=json_util.default))
 
-# data 분석 / mongodb-sensor-data
 @app.task
 def analy(dname, n):
-    # 복합 Query를 위한 Pipeline 구성
     Pipeline = list()
-    Pipeline.append({'$match': {'dev_name': dname, 'error': False}})
-    Pipeline.append({'$sort': {'date': -1}})
-    Pipeline.append({'$limit': int(n)})
-    Pipeline.append(
-        {'$group': {'_id': 'data_analy', 'avg': {'$avg': '$data'}, 'max': {'$max': '$data'}, 'min': {'$min': '$data'}}})
+    Pipeline.append({'$match' : {'dev_name':dname, 'error':False}})
+    Pipeline.append({'$sort' : {'date':-1}})
+    Pipeline.append({'$limit' : int(n)})
+    Pipeline.append({'$group' : {'_id' : 'data_analy', 'avg' : {'$avg':'$data'}, 'max' : {'$max':'$data'}, 'min' : {'$min':'$data'}}})
     res = list(db.data.aggregate(Pipeline))
     return str(json.dumps(res, default=json_util.default))
 
-# 기준시각 이후 data 분석 / mongodb-sensor-data
 @app.task
 def analy_d(date):
     Pipeline = list()
-    Pipeline.append({'$match': {'date': {'$gte': date}}})
-    Pipeline.append(
-        {'$group': {'_id': '$dev_name', 'avg': {'$avg': '$data'}, 'max': {'$max': '$data'}, 'min': {'$min': '$data'}}})
+    Pipeline.append({'$match' : {'date':{'$gte':date}}})
+    Pipeline.append({'$group' : {'_id' : '$dev_name', 'avg' : {'$avg':'$data'}, 'max' : {'$max':'$data'}, 'min' : {'$min':'$data'}}})
     res = list(db.data.aggregate(Pipeline))
     return str(json.dumps(res, default=json_util.default))
 
-# 모든 collection 삭제
 @app.task
 def delete():
     db.data.drop()
